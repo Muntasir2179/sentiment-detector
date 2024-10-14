ff449bd3c9e38970cd8aea10b3a879fec13be161
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-10-14 21:57:11 +0600

    BERT finetuning notebook added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('make-prediction/', views.make_prediction, name='make-prediction'),
-    path('prediction-history/', views.prediction_history,
-         name='prediction-history'),
+    path('prediction-history/', views.prediction_history, name='prediction-history'),
     path('prediction/', views.prediction, name='prediction'),
     path('feedback/', views.feedback, name='feedback')
 ]
